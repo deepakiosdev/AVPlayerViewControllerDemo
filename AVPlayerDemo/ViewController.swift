8fc6c38b7b07256001242b635dd1cfd8e1c9a4dc
Dipak Kumar Pandey <diapk@tabsquare.com>
2021-01-29 22:58:23 +0530

    Initial Commit

---
+++ AVPlayerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayerDemo
+//
+//  Created by Dipak Pandey on 29/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
